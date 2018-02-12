bbaeda4830eed8354e4c3cbe95d9aee66e9dde45
m0rv4i <m0rv4is@gmail.com>
2018-02-12 15:44:51 +0000

    Fix line endings 1

--- cidr-to-list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-if [[ -f "$1" ]]; then
-	nmap -sL -n -iL "$1" | grep "Nmap scan report" | awk '{print $NF}'
-else
-	nmap -sL -n "$1" | grep "Nmap scan report" | awk '{print $NF}'
-fi
